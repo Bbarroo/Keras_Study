35ce168a004452f4e2db02ade2a525b2801f82e2
Bbarroo <bbarroo7425@gmail.com>
2020-10-11 07:39:14 +0000

    2020.10.11, svm completed

--- assignments/assignment1/cs231n/classifiers/linear_svm.py
+++ assignments/assignment1/cs231n/classifiers/linear_svm.py
@@ -36,13 +36,15 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
-
+                dW.T[j] += X[i]
+                dW.T[y[i]] -= X[i]
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
-
+    dW /= num_train
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
+    dW += 2*reg * W
 
     #############################################################################
     # TODO:                                                                     #
@@ -77,7 +79,16 @@ def svm_loss_vectorized(W, X, y, reg):
     # result in loss.                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
 
+    scores = X.dot(W)
+    correct_scores = scores[range(num_train),list(y)].reshape(-1,1)
+    margins = np.maximum(scores - correct_scores +1, 0)
+    margins[range(num_train),list(y)] = 0
+    loss = np.sum(margins)
+    loss /= num_train
+    loss += reg * np.sum(W * W)
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -93,6 +104,12 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+    one_hot_margins = np.where(margins > 0 , 1, margins)
+    one_hot_margins[range(num_train),list(y)] = 0
+    one_hot_margins[range(num_train),list(y)] -= np.sum(one_hot_margins,axis=1)
+    dW = X.T.dot(one_hot_margins)
+    dW /= num_train
+    dW += 2 * reg * W
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
